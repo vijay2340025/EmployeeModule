4cca75803920d74d46367913f5ce866d40db8815
vijay <vijay2340025@gmail.com>
2023-10-02 00:40:33 +0530

    feet: add uri to load from properties

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,12 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.AddressDTO;
 import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
@@ -15,6 +19,17 @@ public class EmployeeService {
 
     @Autowired
     EmployeeRepo employeeRepo;
+
+    static RestTemplateBuilder restTemplateBuilder;
+
+    static String uri;
+
+    @Autowired
+    EmployeeService(RestTemplateBuilder restTemplateBuilder,
+                    @Value("${address-service.root.uri}") String uri) {
+        EmployeeService.restTemplateBuilder = restTemplateBuilder;
+        EmployeeService.uri = uri;
+    }
 
     public ResponseEntity<EmployeeDTO> fetchEmployee(int id) {
         Optional<Employee> employee = employeeRepo.findById(id);
@@ -28,10 +43,21 @@ public class EmployeeService {
 
     private static EmployeeDTO mapEmployeeResponse(Employee res) {
         EmployeeDTO employeeDTO = new EmployeeDTO();
+        AddressDTO addressDTO = new AddressDTO();
+
         employeeDTO.setId(res.getId());
         employeeDTO.setName(res.getName());
         employeeDTO.setEmail(res.getEmail());
         employeeDTO.setDob(res.getDob());
+
+        try {
+            RestTemplate restTemplate = restTemplateBuilder.rootUri(uri + "/address-app").build();
+            addressDTO = restTemplate.getForObject("/address/{id}", AddressDTO.class, res.getId());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        employeeDTO.setAddressDTO(addressDTO);
         return employeeDTO;
     }
 
